d98dc42f0b4fa5d708d6ac61b066c3cf2f4ef124
Gabi <gabi.cassarino@gmail.com>
2018-08-02 18:56:45 -0400

    firebase

--- client/src/components/config/Fire.js
+++ client/src/components/config/Fire.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase';
-
-var config = {
-  apiKey: "AIzaSyCGyfwSt_X1JxdYfNQIA-Zykb1VW3mSxEY",
-  authDomain: "your-perfect-recipes.firebaseapp.com",
-  databaseURL: "https://your-perfect-recipes.firebaseio.com",
-  projectId: "your-perfect-recipes",
-  storageBucket: "your-perfect-recipes.appspot.com",
-  messagingSenderId: "426444960395"
+ // Configure Firebase.
+const config = {
+    apiKey: 'AIzaSyCGyfwSt_X1JxdYfNQIA-Zykb1VW3mSxEY',
+    authDomain: 'your-perfect-recipes.firebaseapp.com',
+    databaseURL: 'https://your-perfect-recipes.firebaseio.com'
+    // ...
 };
-const fire = firebase.initializeApp(config);
-export default fire;
+
+firebase.initializeApp(config);
+export const ref = firebase.database().ref()
+export const firebaseAuth = firebase.auth
+export const reference = firebase
+export default firebase; 
