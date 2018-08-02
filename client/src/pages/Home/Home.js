1c183142d819e8c2aac95b8ed9b5f493b349c28d
Nicole Gould <nkgould@gmail.com>
2018-08-02 18:15:03 -0400

    login

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,50 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import firebase from "../config/constants";
+import fire from './config/Fire';
 
-class NoMatch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
+  class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.logout = this.logout.bind(this);
     }
-  }
 
-  componentDidMount() {
-    this.authListener();
-  }
 
-  authListener() {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if (user) {
-        this.setState({ user });
-        localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({ user: null });
-        localStorage.removeItem('user');
-      }
-    });
-  }
+    logout() {
+        fire.auth().signOut();
+    }
 
-  render() {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Home</h1>
+                <button onClick={this.logout}>Logout</button>
+            </div>
+        );
 
-    return (
-      <div className="App">
-        {this.state.user ? (<Home />) : (<Login />)}
-      </div>
-    );
+    }
 
-    // <Container fluid>
-    //   <Row>
-    //     <Col size="md-12">
-
-    //         <h1>Welcome</h1>
-
-    //     </Col>
-    //   </Row>
-    // </Container>
-  }
 }
 
-export default NoMatch;
+export default Home;
